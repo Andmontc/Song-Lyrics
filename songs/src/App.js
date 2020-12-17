964ba863845f98bc16f791655ec3204116aa6edc
Andmontc <oscarandresmontes@gmail.com>
2020-12-16 19:02:58 -0500

    App component

--- songs/src/App.js
+++ songs/src/App.js
@@ -1,10 +1,35 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Form from './components/form';
+import Axios from 'axios';
 
 function App() {
+
+  const [lyrics, saveLyrics] = useState({});
+  const [lyric, saveLyric ] = useState('');
+
+  useEffect(() => {
+    if (Object.keys(lyrics).length === 0) return;
+    const ApiLyrics = async() => {
+      const {artist, song} = lyrics;
+      const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+      const result = await Axios(url);
+      saveLyric(result.data.lyrics);
+    }
+    ApiLyrics();
+  }, [lyrics])
   return (
     <Fragment>
-      <Form />
+      <Form 
+        saveLyrics={saveLyrics}
+      />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col md-6">
+            
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
